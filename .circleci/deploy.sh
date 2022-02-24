565dc3c4321afe9c1717e3cf09ec9b2641a394c2
m.rostami <m.rostami9710@gmail.com>
2022-02-25 02:43:51 +0330

    pipeline: update/CD - on testnet and mainnet

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,11 @@
 ssh -p $SSH_PORT -T $SSH_USER@$SSH_HOST <<EOA
-ssh -T $SSH_NESTED_HOST <<EOB
+ssh -T $TESTNET_HOST <<EOB
+cd /srv/polido-frontend && git pull && docker-compose build polido-frontend && docker-compose up -d --build polido-frontend
+EOB
+EOA
+
+ssh -p $SSH_PORT -T $SSH_USER@$SSH_HOST <<EOA
+ssh -T $MAINNET_HOST <<EOB
 cd /srv/polido-frontend && git pull && docker-compose build polido-frontend && docker-compose up -d --build polido-frontend
 EOB
 EOA
